eeb00399c5de178424e4eadb51f10a5dea9bae4d
露易娘 <45190797+Ruimusume@users.noreply.github.com>
2024-01-27 01:42:37 +0800

    Update uasset to png Batch.bat

--- uasset to png Batch.bat
+++ uasset to png Batch.bat
@@ -4,6 +4,6 @@ rem 指定好解包有图像的文件目录位置
 set "work_path=C:\Users\Ruimusume\Desktop\x\Pal\Content\Others\InventoryItemIcon\Texture"
 rem 遍历=/r 指定文件夹目录内全部.uasset格式文件，/wait逐个运行png.bat
 for /r "%work_path%" %%i in (*.uasset) do (
-start /wait png.bat "%%i"
+start /wait export_as_png.bat "%%i"
 )
 endlocal
